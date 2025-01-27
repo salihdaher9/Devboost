5f447f24a1d8f1ab30be0c400e2cdfa660bf6dff
salih daher <salihdaher9@gmail.com>
2025-01-27 18:42:14 +0200

    added 348 and solved it

---
+++ 348/348.py
@@ -0,0 +1,26 @@
+# company_simple1.py
+import json
+
+
+def number_of_employees(company):
+    return sum([len(company[i]["members"]) for i in range(len(company))])
+
+
+def total_salaries(company):
+    return sum([company[i]["members"][j]["salary"]for i in range(len(company))for j in range(len(company[i]["members"]))])
+
+
+with open("company2.json") as f:
+    data = json.load(f)
+
+num = number_of_employees(data)
+print("Total employees:", num)
+assert num == 66
+print("OK1")
+
+salary = total_salaries(data)
+print("Total salary:", salary)
+assert salary == 1338800
+print("OK2")
+
+print("Done!")
